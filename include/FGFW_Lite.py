b1a046acc4bf95ea4b925d94f8c68a412010f560
v3aqb <sgzz.cj@gmail.com>
2013-08-03 18:24:52 +0800

    update license info

--- include/FGFW_Lite.py
+++ include/FGFW_Lite.py
@@ -1,13 +1,23 @@
 #!/usr/bin/env python
 #-*- coding: UTF-8 -*-
-#-------------------------------------------------------------------------------
-# Name:        FGFW_Lite.py
-# Purpose:     Fuck the Great Firewall of China
 #
-# Contributer: Jiang Chao <sgzz.cj@gmail.com>
+# FGFW_Lite.py A Proxy Server help go around the Great Firewall
 #
-# License:     The GPLv2 License
-#-------------------------------------------------------------------------------
+# Copyright (C) 2012-2013 Jiang Chao <sgzz.cj@gmail.com>
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; either version 2 of the License, or (at your
+# option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, see <http://www.gnu.org/licenses>.
+
 from __future__ import print_function
 from __future__ import unicode_literals
 
