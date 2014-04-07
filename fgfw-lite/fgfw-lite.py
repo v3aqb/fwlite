59d960b7269157cff1be2e3ca6579cc91c78eaa6
v3aqb <v3aqb>
2014-04-07 22:29:09 +0800

    if goagent appid is set, try goagent twice

--- fgfw-lite/fgfw-lite.py
+++ fgfw-lite/fgfw-lite.py
@@ -981,6 +981,7 @@ class goagentHandler(FGFWProxyHandler):
             goagent.set('gae', 'obfuscate', conf.userconf.dget('goagent', 'obfuscate', '0'))
             goagent.set('gae', 'validate', conf.userconf.dget('goagent', 'validate', '0'))
             goagent.set('gae', 'options', conf.userconf.dget('goagent', 'options', ''))
+            conf.addparentproxy('goagent.', 'http://127.0.0.1:8087 20')
         else:
             logging.warning('GoAgent APPID is NOT set! Fake APPID is used.')
             goagent.set('gae', 'appid', 'dummy')
